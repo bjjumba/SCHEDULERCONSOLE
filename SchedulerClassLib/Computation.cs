4c50932aa7c3120d9555200e16eb84584a4ed91a
jamilah <nantjami@gmail.com>
2024-04-28 21:21:49 +0300

    Removed variables

--- SchedulerClassLib/Computation.cs
+++ SchedulerClassLib/Computation.cs
@@ -3,25 +3,23 @@ namespace SchedulerClassLib;
 public static class Computation
 {
     public static Tuple<DateOnly, string> ComputeNextDate(Config config)
-    { 
-        var occurenceType = config.OccurenceType;
-        var currentDate = config.CurrentDate;
+    {
         DateOnly nextDate = default;
         var description = "";
     
         GuardHelper.ValidateInput(config);
         
-        if (occurenceType == Occurence.Recurring)
+        if (config.OccurenceType == Occurence.Recurring)
         {
-            nextDate = currentDate.AddDays(config.OccursEvery);
+            nextDate = config.CurrentDate.AddDays(config.OccursEvery);
             description = $"Schedule will be used starting on {config.StartDate}";
         }
         
-        else if (occurenceType == Occurence.Once)
+        else if (config.OccurenceType == Occurence.Once)
         {
             DateOnly date = DateOnly.FromDateTime(config.ConfDateTime);
             TimeOnly time = TimeOnly.FromDateTime(config.ConfDateTime);
-            nextDate = currentDate.AddDays(+1);
+            nextDate = config.CurrentDate.AddDays(+1);
             description = $"Schedule will be used on {date} at {time} starting on {config.StartDate}";
         }
         return Tuple.Create(nextDate, description)!;
