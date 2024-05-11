b6bcc7870a9eb43074dcaf4fb6a51cf6fb4beb14
jamilah <nantjami@gmail.com>
2024-05-11 15:57:16 +0300

    Added the CurrentDate before StartDate test

--- SchedulerTest/RecurringTest.cs
+++ SchedulerTest/RecurringTest.cs
@@ -26,6 +26,21 @@ public class SchedulerTest
     [Fact]
     public void CurrentDate_Is_Before_StartDate()
     {
+        //Arrange
+        var expectedDate = new DateOnly(2020, 1, 4);
+            
+        //Act
+        var result = Computation.ComputeNextDate(new Config
+        {
+            ConfDateTime = new DateTime(2024, 5, 4, 3, 40, 5),
+            CurrentDate = new DateOnly(2020, 1, 1),
+            OccurenceType = Occurence.Recurring,
+            OccursEvery = 1,
+            StartDate = new DateOnly(2020, 1, 4),
+            EndDate = new DateOnly(2020, 1, 8),
+        });
         
+        //Assert
+        Assert.Equal(expectedDate, result.Item1);
     }
 }
